98787ae756f4d8fcbc0593776774c9ab35943bcb
Nick Kaczmarek <nickkaczmarek@me.com>
2024-04-27 12:55:03 -0400

    Add menu bar app that can show and hide overlay. - nk
    [main]

--- Sunglasses/AppDelegate.swift
+++ Sunglasses/AppDelegate.swift
@@ -10,11 +10,14 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
+    var statusBarItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
 
         setUpWindow()
+
+        setUpStatusBar()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -40,6 +43,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.window.level = .floating
         self.window.backgroundColor = NSColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 0.2)
         self.window.orderFrontRegardless()
+    }
+
+    private func setUpStatusBar() {
+        let statusBar = NSStatusBar.system
+
+        self.statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+        self.statusBarItem.button?.title = "🕶️"
+
+        let statusBarMenu = NSMenu(title: "Sunglasses Status Bar Menu")
+        self.statusBarItem.menu = statusBarMenu
+
+        statusBarMenu.addItem(
+            NSMenuItem(
+                title: "Hide Sunglasses",
+                action: #selector(AppDelegate.hideSunglasses),
+                keyEquivalent: ""
+            )
+        )
+
+        statusBarMenu.addItem(
+            NSMenuItem(
+                title: "Show Sunglasses",
+                action: #selector(AppDelegate.showSunglasses),
+                keyEquivalent: ""
+            )
+        )
+    }
+
+    @objc func hideSunglasses() {
+        NSApplication.shared.hide(self)
+    }
+
+    @objc func showSunglasses() {
+        NSApplication.shared.unhide(self)
     }
 
 }
